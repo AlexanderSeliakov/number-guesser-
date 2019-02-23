033ee8639f5322794b71bb2ae3cee0d8951319b7
AlexanderSeliakov <46649919+AlexanderSeliakov@users.noreply.github.com>
2019-02-23 19:24:40 +0200

    Add files via upload
    simple console app

---
+++ gs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace gs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //difining our class
+            numberGuesser ng = new numberGuesser();
+
+            //difining the range
+            ng.MaximumNumber = 200;
+
+            //ask a user to think of a number
+            ng.AskUser();
+
+            //guessing the number
+            ng.NumGuesser();
+
+            //anounce the result
+            Console.WriteLine($"*****your Num is {ng.MinimumNumber}*****");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ gs/numberGuesser.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gs
+{
+    /// <summary>
+    /// Ask the user to guess a number between range we made
+    /// </summary>
+    public class numberGuesser
+    {
+        #region guess range prorerties
+
+        /// <summary>
+        /// the larger number we ask the user to guess
+        /// </summary>
+        public int MaximumNumber { get; set;} = 200;
+
+        /// <summary>
+        /// the current known minimun number the user is thinking of
+        /// </summary>
+        public int MinimumNumber { get; private set; }
+
+        /// <summary>
+        /// the current known maximum number  the user is thinking of
+        /// </summary>
+        public int Guess { get; private set; }  //guessing step
+
+        #endregion
+
+        public numberGuesser()
+        {
+            
+            MaximumNumber = 200;
+
+        }
+
+        /// <summary>
+        /// ask user to think of a number
+        /// </summary>
+        public void AskUser()
+        {
+            //ask user a num
+            Console.WriteLine($"guess the number between {MinimumNumber} and {MaximumNumber} and press Y as 'Yes' and N as 'No'");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// series of questions to figure out the number
+        /// </summary>
+        public void NumGuesser()
+        {
+            //defining gusser num
+            Guess = MaximumNumber / 2;
+
+            //guessing loop
+            while (MinimumNumber != MaximumNumber)
+            {
+
+                Console.WriteLine($"is your num between {MinimumNumber} and {Guess}");
+                string response = Console.ReadLine();
+                // '?.' means is = 0; FirstOtDefault checks 1st char 
+                if (response?.ToLower().FirstOrDefault() == 'y')
+                {
+                    MaximumNumber = Guess;
+                    Guess = Guess - (Guess - MinimumNumber) / 2;
+
+                }
+                else
+                {
+                    MinimumNumber = Guess + 1;
+                    int remainingDifference = MaximumNumber - Guess;
+                    Guess = MaximumNumber - remainingDifference / 2;
+                }
+
+                // if 2 nims left
+                if (MinimumNumber + 1 == MaximumNumber)
+                {
+                    Console.WriteLine($"is your num {MinimumNumber}");
+                    response = Console.ReadLine();
+                    if (response?.ToLower().FirstOrDefault() == 'y')
+                    {
+                        break;
+
+                    }
+                    else
+                    {
+                        MinimumNumber = MaximumNumber;
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
